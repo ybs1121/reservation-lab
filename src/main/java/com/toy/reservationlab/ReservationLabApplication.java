7a49d416e48e3577a9bc4c56f826b15b0a407661
ybs1121 <ybs991@naver.com>
2026-05-08 21:32:06 +0900

    init

---
+++ src/main/java/com/toy/reservationlab/ReservationLabApplication.java
@@ -0,0 +1,13 @@
+package com.toy.reservationlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationLabApplication.class, args);
+    }
+
+}
